d4b773ac9cac60c940af53ce65c9284c21b458e6
bentheier <bent.heier@gmail.com>
2017-10-20 20:06:36 +0200

    Finished 2 pluss some 3

--- ir3.py
+++ ir3.py
@@ -3,6 +3,7 @@ import codecs
 import string
 import re
 from nltk.stem.porter import PorterStemmer
+import gensim
 
 f = codecs.open("pg3300.txt", "r", "utf-8")
 
@@ -23,4 +24,28 @@ for w in words:
 
 stemmer = PorterStemmer()
 word = "Economics"
-print(stemmer.stem(word.lower()))
+dictionary = gensim.corpora.Dictionary(words)
+
+s = codecs.open('common-english-words.txt', "r", "utf-8")
+stopwords = s.read().split(',')
+stop_ids = []
+
+for stopword in stopwords:
+    #stops at the last characther
+    if stopword == '\n':
+        break
+    try:
+        stopword_id = dictionary.token2id[stopword]
+        stop_ids.append(stopword_id)
+        dictionary.filter_tokens(stop_ids)
+        #skips stopwords not in dictionary
+    except KeyError:
+        pass
+
+corpus = [dictionary.doc2bow(text) for text in words]
+
+tfidf_model = gensim.models.TfidfModel(corpus)
+tfidf_corpus = tfidf_model[corpus]
+lsi = gensim.models.LsiModel(corpus, id2word=dictionary, num_topics=2)
+index = gensim.similarities.MatrixSimilarity(lsi[corpus])
+print(index)
